413b606907cef175a56bf632ab6d8a41a5d25b99
EllieZora <isabelpriorecoelho@gmail.com>
2025-02-18 00:41:55 -0500

    add more tests

--- tests/item/item-store_test.go
+++ tests/item/item-store_test.go
@@ -12,12 +12,21 @@ func TestGetItem(t *testing.T) {
 	item2 := item.Item{Code: "02", Category: "Cat 2", Name: "Item 2", Description: "test 2"}
 	store := item.NewStore([]*item.Item{&item0, &item1, &item2})
 
-	existingItem, ok := store.GetItem("00")
+	existingItem0, ok := store.GetItem("00")
 	if !ok {
 		t.Errorf("Item with code 00 should be retrievable")
 	} else {
-		if existingItem.Code != "00" {
-			t.Errorf("code does not match: got %v want %v", existingItem.Code, "00")
+		if existingItem0.Code != "00" {
+			t.Errorf("code does not match: got %v want %v", existingItem0.Code, "00")
+		}
+	}
+
+	existingItem1, ok := store.GetItem("03")
+	if !ok {
+		t.Errorf("Item with code 03 should be retrievable even if not in cache")
+	} else {
+		if existingItem1.Code != "03" {
+			t.Errorf("code does not match: got %v want %v", existingItem1.Code, "03")
 		}
 	}
 }
